e0f2284736e83d9542dae6e42c9a0907a5fa1adc
unknown <turar.abishev01@gmail.com>
2019-02-03 23:06:57 +0600

    task1

---
+++ week1/task1/task1/Task1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            string g = Console.ReadLine();
+            int a = int.Parse(g); // creating integer from string
+
+            string l = Console.ReadLine();
+            string[] arr = l.Split();// creating array from string
+            for (int i = 0; i < a; i++)// started cycle
+            {
+                float b = int.Parse(arr[i]);// changes integers in array to float
+                for (int j = 2; j <= Math.Sqrt(b); j++)// loop to check if the particular number has divisors
+                {
+                    if (b % j == 0 && b != j)
+                    {
+                        b = 0;
+                        break;
+                    }
+                }
+                if (b != 0 && b != 1)
+                {
+                    Console.Write(b + " ");// outputting the elements with space
+                }
+
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
