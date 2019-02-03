c33def9427d08f3dc83079036446ff03916dd382
unknown <turar.abishev01@gmail.com>
2019-02-03 23:11:27 +0600

    Task4

---
+++ week1/Task4/Task4/Task4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string d = Console.ReadLine();
+            int n = int.Parse(d);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j - i <= 0)
+                    {
+                        Console.Write("[*]");
+                    }
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
