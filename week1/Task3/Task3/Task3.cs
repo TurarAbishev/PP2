466da64e4a04b8c2046264f1b5dd4b659007d35c
unknown <turar.abishev01@gmail.com>
2019-02-03 23:09:38 +0600

    Task3

---
+++ week1/Task3/Task3/Task3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Task3
+    {
+        static void Main(string[] args)
+        {
+            string d = Console.ReadLine();// enter string d
+            int a = int.Parse(d);// changes string to integer
+            string s = Console.ReadLine();// enter string s
+            string[] arr = s.Split();// creating array from string
+            for (int i = 0; i < a; i++)// cycle run            
+                {
+                for (int j = 0; j < 2; j++)// cycle 2 times run
+                {
+                    Console.Write(arr[i] + " ");// cout 2 same integers
+                }
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
