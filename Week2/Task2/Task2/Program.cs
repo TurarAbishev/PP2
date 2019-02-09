a7320cab6f063fa61c820e2de571e38fb6c2754b
unknown <turar.abishev01@gmail.com>
2019-02-09 15:42:25 +0600

    task1

---
+++ Week2/Task2/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string g = System.IO.File.ReadAllText(@"C:\Users\Пользователь\Desktop\pp\PP2\Week2\Task2\ex1.txt");
+            string[] arr = g.Split();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                float d = float.Parse(arr[i]);
+                for (int j = 2; j <= Math.Sqrt(d); j++)
+                {
+                    if (d % j == 0)
+                    {
+                        d = 0;
+                        break;
+                    }
+                }
+                if (d != 0 && d != 1)
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Пользователь\Desktop\pp\PP2\Week2\Task2\ex2.txt", true))
+                    {
+                        file.Write(d + " ");
+                    }
+                }
+
+            }
+        }
+    }
+}
